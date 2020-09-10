2fc08073a4c12b45bac5c4dba92e25e368abb06c
rakibtg <rakibtg@gmail.com>
2020-09-10 17:24:21 +0600

    Added CORS support on dev mode

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 const HOST = "localhost";
 const PORT = 5500;
 
+if (process.env.NODE_ENV === "development") {
+  app.use(cors());
+}
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
